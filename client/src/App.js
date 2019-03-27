26cede5eb7cf350406c4d22c102097471d42e46a
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos.local>
2019-03-27 16:03:30 +0100

    signup-login-react

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Signup from './components/auth/Signup';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = { loggedInUser: null };
+  }
+
+  getTheUser= (userObj) => {
+    this.setState({
+      loggedInUser: userObj
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+        <Switch>
+  <Route exact path="/signup" component={Signup}/>
+  
+  
+</Switch>
       </div>
     );
   }
