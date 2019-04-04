c8619d0cf6f6d870b953ffd0f124d8c35d960254
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos.local>
2019-04-04 18:56:05 +0200

    to heroku again

---
+++ server/public/precache-manifest.3f92640f1c4803d47b7552573866440d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d0e04eb337a5fb0b54db",
+    "url": "/static/js/main.4b1b2a3d.chunk.js"
+  },
+  {
+    "revision": "6c5b7f0e022d31ab5061",
+    "url": "/static/js/2.338648ba.chunk.js"
+  },
+  {
+    "revision": "d0e04eb337a5fb0b54db",
+    "url": "/static/css/main.af0debd4.chunk.css"
+  },
+  {
+    "revision": "bc6be4f9c60db425ade5806551e56b2e",
+    "url": "/index.html"
+  }
+];
