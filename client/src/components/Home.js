4466daa43fd01add15ce3e6ada4889040af7dfab
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos.local>
2019-04-04 10:17:26 +0200

    video background init

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,20 +2,18 @@ import React from "react"
 
 
 
+const Home = () =>{
 
-const Home = ()=>{
 
 
 return(
     <div>
-
-
-
-
-
+        <video autoPlay="autoplay" loop="loop" id="video_background" muted>
+            <source src="https://res.cloudinary.com/dqphzmuq1/video/upload/v1554364685/paintings/A0450_1507_H3_807_1163_Videvo.mp4" type="video/mp4" />
+        </video>
     </div>
 )
-
 }
+
 
 export default Home
--- server/routes/obras.js
+++ server/routes/obras.js
@@ -51,9 +51,18 @@ router.get("/obras", (req, res) =>{
     })
 
 router.post("/postobra",uploader.single("obra"), (req, res)=>{  
- User.findByIdAndUpdate(req.user.id, {$push:{obras:req.file.secure_url}})
-   .then(data =>  res.status(200).json(data))
-   .catch(err=>console.log(err))
+
+    const {secure_url} = req.file;
+    const newObra = new ObraMaestra({
+        image: secure_url
+    })
+    newObra.save()
+    .then(obra => {
+        console.log(req.user)
+        User.findByIdAndUpdate(req.user._id, {$push:{obras:obra._id}})
+          .then(data =>  res.status(200).json(data))
+        })
+        .catch(err=>console.log(err))
 })
 
 
