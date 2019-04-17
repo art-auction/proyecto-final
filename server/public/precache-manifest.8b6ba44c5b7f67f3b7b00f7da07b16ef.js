ef2f1400750f1be711bdc5db80de2532301a1ad0
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos-2.local>
2019-04-17 14:38:54 +0200

    to heroku day3 yyay

---
+++ server/public/precache-manifest.8b6ba44c5b7f67f3b7b00f7da07b16ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca547bd2d0afeffe4403",
+    "url": "/static/js/main.7bd26366.chunk.js"
+  },
+  {
+    "revision": "4627e71ce235e2359816",
+    "url": "/static/js/2.b09932bd.chunk.js"
+  },
+  {
+    "revision": "ca547bd2d0afeffe4403",
+    "url": "/static/css/main.81b59818.chunk.css"
+  },
+  {
+    "revision": "3ba1cf2a2c60af9374b4919aa1f9f313",
+    "url": "/index.html"
+  }
+];
